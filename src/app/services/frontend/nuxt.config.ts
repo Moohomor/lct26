f4b2253ba0bed451e6e59efe16f4300f85bdc5c7
petrovk <petrovk86@mail.ru>
2026-09-21 21:14:17 +0400

    feat: добавлены стили

--- src/app/services/frontend/nuxt.config.ts
+++ src/app/services/frontend/nuxt.config.ts
@@ -2,6 +2,12 @@
 export default defineNuxtConfig({
   modules: ['@pinia/nuxt'],
 
+  css: [
+    '~/assets/css/font-face.css',
+    '~/assets/css/app.css',
+    '~/assets/css/footer.css',
+  ],
+
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: false,
