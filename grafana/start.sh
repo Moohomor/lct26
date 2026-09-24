8fc6af4aa1c4adfb2803d0d8bd39e30e97dc3424
Марк Каргин <cpp@java.com>
2026-09-24 22:45:02 +0300

    fix(grafana): переносим порт $PORT в entrypoint образа вместо dockerCommand

---
+++ grafana/start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+# Render ожидает, что приложение слушает порт из переменной окружения $PORT,
+# а Grafana по умолчанию слушает 3000. Подменяем порт при старте.
+export GF_SERVER_HTTP_PORT="${PORT:-3000}"
+
+# Оригинальный entrypoint образа grafana/grafana
+exec /run.sh "$@"
